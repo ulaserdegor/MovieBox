cadbd0128d4fb09347cf9783598db670bb3370c7
Murat Can BUR <muratcanbur@gmail.com>
2016-02-06 12:44:27 +0200

    added simple travis test support.

--- app/src/main/java/co/mobiwise/moviebox/ui/activity/MainActivity.java
+++ app/src/main/java/co/mobiwise/moviebox/ui/activity/MainActivity.java
@@ -2,10 +2,8 @@ package co.mobiwise.moviebox.ui.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -21,13 +19,6 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -51,5 +42,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
